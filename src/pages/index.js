9ab88c8d4f329d4920c6f25533fc647566b0b648
ggakila <gisorehaggai@gmail.com>
2023-10-02 13:43:01 +0300

    more edits to ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Header from "@/components/Header"
 import Previewcontent from "@/components/Previewcontent"
+import Savevid from "@/components/Savevid"
 export default function Home() {
   return (
-    <div className="h-screen w-screen px-[100px] flex flex-col items-center">
+    <div className="h-screen w-screen overflow-x-hidden px-[100px] flex flex-col items-center gap-[40px]">
       <Header />
       <Previewcontent/>
+      <Savevid/>
       
     </div>
   )
